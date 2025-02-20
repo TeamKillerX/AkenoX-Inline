6155e5329c05a9423a071a2b051ead20b8193e63
Randy Dev <killerx@randydev.my.id>
2025-02-20 23:53:33 +0700

    Create admin.py

---
+++ AkenoX/plugins/admin.py
@@ -0,0 +1,48 @@
+from AkenoX.plugins.libso.funcs_admin import *
+from AkenoX import *
+
+@RENDYDEV.user(prefix=["promote", "fullpromote"], filters=(filters.me & ~filters.forwarded))
+async def promoted_handler(client, message):
+    await promotte_user(client, message)
+
+@RENDYDEV.user(prefix=["demote"], filters=(filters.me & ~filters.forwarded))
+async def demote_handler(client, message):
+    await demote_user(client, message)
+
+@RENDYDEV.user(prefix=["ban", "dban"], filters=(filters.me & ~filters.forwarded))
+async def member_ban_handler(client, message):
+    await member_ban_user(client, message)
+
+@RENDYDEV.user(prefix="unmute", filters=(filters.me & ~filters.forwarded))
+async def unmute_handler(client, message):
+    await unmute_user(client, message)
+
+@RENDYDEV.user(prefix="pin", filters=(filters.me & ~filters.forwarded))
+async def pin_handler(client, message):
+    await pin_message(client, message)
+
+@RENDYDEV.user(prefix=["mute", "dmute"], filters=(filters.me & ~filters.forwarded))
+async def mute_handler(client, message):
+    await mute_user(client, message)
+
+@RENDYDEV.user(prefix=["kick", "dkick"], filters=(filters.me & ~filters.forwarded))
+async def kick_handler(client, message):
+    await kick_user(client, message)
+
+RENDYDEV.buttons(
+    "admin",
+    [
+        ["ban [reply/username/userid]", "Ban someone."],
+        ["dban [reply]", "dban a user deleting the replied to message."],
+        ["unban [reply/username/userid]", "Unban someone."],
+        ["kick [reply/username/userid]", "kick out someone from your group."],
+        ["dkick [reply]", "dkick a user deleting the replied to message."],
+        ["promote `or` .fullpromote", "Promote someonen."],
+        ["demote", "Demote someone."],
+        ["mute [reply/username/userid]", "Mute someone."],
+        ["dmute [reply]", "dmute a user deleting the replied to message."],
+        ["unmute [reply/username/userid]", "Unmute someone."],
+        ["pin [reply]", "to pin any message."],
+        ["unpin [reply]", "To unpin any message."],
+    ],
+)
